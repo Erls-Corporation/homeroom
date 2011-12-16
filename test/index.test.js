7e7ec572a8fd7e45154127f468e064b3b4505e7f
Edward Hotchkiss <e@ingk.com>
2011-12-15 21:38:53 -0800

    basic prototypes

--- test/index.test.js
+++ test/index.test.js
@@ -11,12 +11,30 @@ var homeroom = require("../lib/homeroom");
 vows.describe("General Module Tests").addBatch({
   "when instantiating homeroom" : {
     topic : function() { 
-      
       return homeroom;
     },
     "homeroom should be a function" : function(topic) {
       topic.should.be.a("function");
     },
+  },
+  "when creating a class called mathematics with a method multiple" : {
+    topic : function() {
+      var mathematics = new homeroom("mathematics", {
+        __constructor() : function() {
+          console.log("class instantiated");
+        },
+        __destructor() : function() {
+          console.log("class destroyed");
+        },
+        multiply : function(a, b) {
+          return a*b;
+        }
+      });
+      return mathematics.multiply(10, 10);
+    },
+    "mathematics.multiply(10, 10) should return 100" : function(topic) {
+      assert.equal(topic, 100);
+    }
   }
 }).export(module);
 
